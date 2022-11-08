8c3d736b7dcfdbcbadba821c60db1191403d2a51
Felisha <felisha.j.yu@gmail.com>
2022-11-08 11:05:45 -0800

    added basic handlebars for all pages and linked js files

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
-//const exphbs = require('express-handlebars');
+const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -10,11 +10,11 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Create the Handlebars.js engine object with custom helper functions
-//const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({ helpers });
 
 // Inform Express.js which template engine we're using
-//app.engine('handlebars', hbs.engine);
-//app.set('view engine', 'handlebars');
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 const sess = {
   secret: 'Super secret secret',
