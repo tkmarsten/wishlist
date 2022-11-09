c634def3181baff079ee38a06bee3956b084abcc
Felisha <felisha.j.yu@gmail.com>
2022-11-09 15:47:42 -0800

    random list link brings up random wish list

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { Wishlist, Item, User } = require('../models');
+const wishlistData = require('../seeds/wishlist.json')
 
 // GET homepage
 router.get('/', async (req, res) => {
@@ -33,5 +34,25 @@ router.get("/profile", (req, res) => {
     res.render("profile", hbsData)
   })
 })
+
+
+router.get("/random", (req,res) => {
+    const randomID = Math.floor(Math.random() * wishlistData.length)
+    console.log(randomID)
+  Wishlist.findByPk( randomID ,
+  {
+  include: [Item]
+}
+  ).then(
+    listData => {
+  //   // const hbsData = listData.toJSON();
+  //   console.log(listData)
+  //   // res.render("list-details", hbsData)
+    res.render("list-details",listData)
+  }
+  )
+  });
+
+
 
 module.exports = router
