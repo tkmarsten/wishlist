f9976c1e1022a7e85ccc5e82cf1b6fea10e425cb
tkmarsten <tkmarsten@gmail.com>
2022-11-08 22:07:27 -0800

    Add wishlist table

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,7 +1,8 @@
 const sequelize = require('../config/connection');
-const { User, Item } = require('../models');
+const { User, Wishlist, Item } = require('../models');
 
 const userData = require('./user.json');
+const wishlistData = require('./wishlist.json')
 const itemData = require('./item.json');
 
 const seedDatabase = async () => {
@@ -12,11 +13,17 @@ const seedDatabase = async () => {
     returning: true,
   });
 
+  for (const wishlist of wishlistData) {
+    await Wishlist.create({
+      ...wishlist,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+
   for (const item of itemData) {
     await Item.create({
       ...item,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
-    });
+    })
   }
 
   process.exit(0);
