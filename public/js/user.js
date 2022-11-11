73756699e79afc17374ba73c48120a3335d28907
Felisha <felisha.j.yu@gmail.com>
2022-11-10 16:32:15 -0800

    added search by user id able to get username from search but can't find by username

--- public/js/user.js
+++ public/js/user.js
@@ -1,2 +1,46 @@
+const { User } = require('../../models')
 console.log("user.js linked")
 
+// const searchForm = document.querySelector("#searchUser");
+// searchForm.addEventListener("submit", e => {
+//     e.preventDefault();
+//     console.log('PREVENTED DEFAULT!')
+//     const userID =document.querySelector("#searchUsername").value
+//     console.log(`userID ${userID}`)
+//     fetch("/api/users/", {
+//         method: "GET",
+//         id: userID
+//     }).then(res => {
+//         if (res.ok) {
+//             console.log(res)
+//             location.replace(`/api/users/${userID}`)
+//         } else {
+//             alert("error finding user")
+//         }
+//     })
+// })
+
+
+const searchForm = document.querySelector("#searchUser");
+searchForm.addEventListener("submit", e => {
+    e.preventDefault();
+    console.log('PREVENTED DEFAULT!')
+    const userName =document.querySelector("#searchUsername").value
+    console.log(`username is ${userName}`)
+    // fetch("/api/users/", {
+    //     method: "GET",
+    //     where: {
+    //         username: userName
+    //       }
+    // })
+})
+// .then(res => {
+//     if (res.ok) {
+//         console.log(res)
+//         // location.replace(`/api/users/${username}`)
+//     } else {
+//         alert("error finding user")
+//     }
+// })
+
+   
