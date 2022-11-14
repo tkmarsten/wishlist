2343304dae309a320539a45ea035114427aefdf0
Felisha <felisha.j.yu@gmail.com>
2022-11-14 10:06:09 -0800

    got random number fixed

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -41,55 +41,23 @@ router.get("/profile", (req, res) => {
 
 router.get("/random", (req,res) => {
   function getRandomWishlist () {
-    const randomID = Math.floor(Math.random()*10 + 1)
+    const randomID = Math.floor(Math.random()*100 + 1)
     console.log(randomID)
     Wishlist.findByPk( randomID ,
       {include: {all:true}}
   ).then(listData=>{
-    if (!listData) {
-      getRandomWishlist()
+    if (listData) {
+      const listDataHbsData = listData.get({plain:true});
+      // console.log(listDataHbsData)
+      return res.render("list-details",listDataHbsData)
+      
     }
-    const listDataHbsData = listData.get({plain:true});
-    // console.log(listDataHbsData)
-    res.render("list-details",listDataHbsData)
+    getRandomWishlist()
+
   })
 }
-  getRandomWishlist()
+getRandomWishlist()
 })
-
-// //get random id from 1-10 redirect to 1 if doesn't exist
-// router.get("/random", (req,res) => {
-  //   const randomID = Math.floor(Math.random()*10 + 1)
-  //   console.log(randomID)
-  // Wishlist.findByPk( randomID ,
-  // {include: {all:true}}
-// ).then(listData=>{
-  //   if (!listData) {
-    //     return res.redirect('/api/wishlists/1')
-    //   }
-    //   const listDataHbsData = listData.get({plain:true});
-    //   // console.log(listDataHbsData)
-    //   res.render("list-details",listDataHbsData)
-    // })
-    // })
-    
-// //get random using seed data
-//     //get seed data
-//     const wishlistSeedData = require('../seeds/wishlist.json')
-
-//   router.get("/random", (req,res) => {
-//     const randomID = Math.floor(Math.random() * wishlistSeedData.length+1)
-//     console.log(wishlistSeedData)
-//     console.log(wishlistSeedData.length)
-//     console.log(randomID)
-//   Wishlist.findByPk( randomID ,
-//   {include: {all:true}}
-//   ).then(listData=>{
-//     const listDataHbsData = listData.get({plain:true});
-//     console.log(listDataHbsData)
-//     res.render("list-details",listDataHbsData)
-// })
-// })
 
 //all users
 router.get("/viewallusers", (req,res) => {
